9daeaded1ecc28b0291ee64a8d1a1967a4090fa8
Erdemhasates35 <Erdemhasates343@gmail.com>
2026-05-12 14:19:32 +0300

    FIX: Erdemhasates35 tarafından reel veri onarımı ve söz dizimi düzeltmesi tamamlandı

--- nexus_doctor.py
+++ nexus_doctor.py
@@ -1,6 +1,6 @@
 import os
 import subprocess
-import shutil
+
 
 class RealWorldHealer:
     def __init__(self):
@@ -10,9 +10,10 @@ class RealWorldHealer:
         # Gerçek veri: Fiziksel dosya sistemini tara ve onar
         subprocess.run(["ruff", "check", self.root, "--fix"], capture_output=True)
         subprocess.run(["black", self.root], capture_output=True)
-        
+
         # Gereksiz önbellek (cache) dosyalarını temizle
         os.system(f"find {self.root} -name '__pycache__' -type d -exec rm -rf {{}} +")
+
 
 if __name__ == "__main__":
     healer = RealWorldHealer()
